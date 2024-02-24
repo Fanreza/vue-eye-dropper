1e86c7bb03c836324c9abf6b9f03afe4ee0cc974
Muhamad Jamil <rezaramdhani461@gmail.com>
2024-02-24 22:47:11 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import EyeDropper from './components/EyeDropper.vue'
+</script>
+
+<template>
+  <EyeDropper />
+</template>
---
+++ src/components/EyeDropper.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+
+interface Props {
+  defaultColor?: string
+  width?: number
+  height?: number
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  defaultColor: '#fff',
+  width: 25,
+  height: 25
+})
+
+const colorPick = ref<string>('')
+const colorValue = computed(() => colorPick.value || props.defaultColor || '#fff')
+
+interface ResultDropper {
+  sRGBHex: string
+}
+
+const pickDrop = () => {
+  // @ts-ignore
+  const eyedropper = new EyeDropper()
+
+  eyedropper.open().then((result: ResultDropper) => {
+    colorPick.value = result?.sRGBHex
+  })
+}
+</script>
+
+<template>
+  <button @click="pickDrop"></button>
+</template>
+
+<style scoped>
+button {
+  width: v-bind(width + 'px');
+  height: v-bind(height + 'px');
+  background-color: v-bind(colorValue);
+  border-radius: 25%;
+  border: none;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  border: 1px solid #000;
+}
+</style>
